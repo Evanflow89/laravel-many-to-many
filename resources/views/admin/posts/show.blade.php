74968f1b28954e0cd6da3de0c9957f2dd6156a72
Evanflow89 <ivanbaragone@gmail.com>
2022-07-23 13:05:33 +0200

    wip

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,11 +6,24 @@
     <div class="card">
         <div class="card-header">
             <h1>{{$post->title}}</h1>
+            @if ($post->category)
+               <span class="badge badge-primary">{{$post->category->name}}</span>
+            @endif
         </div>
         <div class="card-body">
             <div>{{$post->content}}</div>
             <a class="btn btn-primary mt-3" href="{{route('admin.posts.index')}}">Back</a>
+            @if (count($post->tags) > 0)
+            <div class="mt-3">
+                <h5>Tags</h5>
+                <ul>
+                @foreach ($post->tags as $tag)
+                    <li>{{$tag}}</li>
+                @endforeach
+            </ul>
+            </div>
         </div>
+        @endif
     </div>
 </div>
 @endsection
